d2ece29aa4a93c45f7fbb09d143d47122cdba183
Adilson <asu2u@yahoo.com.br>
2017-05-26 00:54:55 -0300

    Criando componentes e dividindo o html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './css/reset.css';
+import './css/timeline.css';
+import './css/login.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
